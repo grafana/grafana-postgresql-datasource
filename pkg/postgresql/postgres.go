72236c8a6a0016a1bc001d22c70ac63e7a7af3fd
Nathan Verzemnieks <njvrzm@gmail.com>
2026-05-11 17:33:32 +0200

    Get GrafanaConfigFromContext from new location

--- pkg/postgresql/postgres.go
+++ pkg/postgresql/postgres.go
@@ -12,6 +12,7 @@ import (
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/datasource"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/instancemgmt"
+	"github.com/grafana/grafana-plugin-sdk-go/config"
 	"github.com/grafana/grafana-plugin-sdk-go/data"
 	"github.com/grafana/grafana-plugin-sdk-go/data/sqlutil"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -78,7 +79,7 @@ func newPostgres(ctx context.Context, userFacingDefaultError string, rowLimit in
 
 func NewInstanceSettings(logger log.Logger) datasource.InstanceFactoryFunc {
 	return func(ctx context.Context, settings backend.DataSourceInstanceSettings) (instancemgmt.Instance, error) {
-		cfg := backend.GrafanaConfigFromContext(ctx)
+		cfg := config.GrafanaConfigFromContext(ctx)
 		sqlCfg, err := cfg.SQL()
 		if err != nil {
 			return nil, err
